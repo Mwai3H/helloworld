a455168f03333f5c0bca3addb9c42645ebe3ee7f
elijah mwai <emwai9@gmail.com>
2019-02-11 15:43:18 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+var identifier = prompt('identify yourself');
+if (identifier.toLowerCase()==="kamau"){
+    alert('niaje kamau!');
+}else if(identifier.toLowerCase()==="mwangi"){
+    alert('niaje mwangi!');
+}else{
+    alert('get lost');
+}
